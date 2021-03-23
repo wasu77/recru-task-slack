f3ec16e5b95d5528a9c893b6d9ec3cde9a9a84b0
mwezyk <maciek.wezyk@gmail.com>
2021-03-23 21:32:10 +0100

    Scaffolding -  SpringBoot project created with spring initializr

---
+++ src/main/java/pl/mwezyk/insult/InsultApplication.java
@@ -0,0 +1,13 @@
+package pl.mwezyk.insult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruInsultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mwezyk/insult/InsultApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwezyk.insult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
